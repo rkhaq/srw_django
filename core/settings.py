83e2184e53b0fa092f8e66b7a2c612a70a884c75
rkhaq <rqk.email2@gmail.com>
2023-04-08 23:09:44 +1000

    update cors config

--- core/settings.py
+++ core/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -136,8 +135,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Configure CORS
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # Replace with your Next.js frontend domain
+    os.environ.get("FRONTEND_DOMAIN"),  # Replace with your Next.js frontend domain
 ]
+CORS_ALLOW_CREDENTIALS = True
 
 # Configure heroku
 import dj_database_url
