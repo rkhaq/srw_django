f27b675c37c125fd0b62add359f629ae5b7a84d1
rkhaq <rqk.email2@gmail.com>
2023-04-09 01:32:19 +1000

    edit cors allowed domain

--- core/settings.py
+++ core/settings.py
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Configure CORS
 CORS_ALLOWED_ORIGINS = [
-    os.environ.get("FRONTEND_DOMAIN"),  # Replace with your Next.js frontend domain
+    os.environ.get("FRONTEND_DOMAIN", "http://localhost:3000"),  # Replace with your Next.js frontend domain
 ]
 CORS_ALLOW_CREDENTIALS = True
 
